f5ea7ab15fc58e4e5e6e9b95612c52557dd6bec4
Ariya-Min <107289107+Ariya-Min@users.noreply.github.com>
2022-06-11 10:37:24 +0800

    Add files via upload
    version 001

---
+++ lstm.py
@@ -0,0 +1,210 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda
+#data_csv = pd.read_csv('C:\\Users\\86134\\Desktop\\temp.csv', header=None,usecols=[1])
+from torch.utils.data import Dataset
+import os
+from torch.autograd import Variable
+
+# class CustomDataset(Dataset):
+#     def __init__(self,csv_dir, transform=None, target_transform=None):
+#         self.csv = pd.read_csv(csv_dir,usecols=[1])
+#         self.label = self.csv.iloc[:,1]
+#         self.csv_dir = csv_dir
+#         self.transform = transform
+#         self.target_transform = target_transform
+#
+#     def __len__(self):
+#         return len(self.label)
+#
+#     def __getitem__(self, idx):
+#         return idx,self.csv.iloc[idx,1]
+# def create_dataset(dataset, look_back=2):
+#     dataX, dataY = [], []
+#     for i in range(len(dataset) - look_back):
+#         a = dataset[i:(i + look_back)]
+#         dataX.append(a)
+#         dataY.append(dataset[i + look_back])
+#     return [np.array(dataX), np.array(dataY)]
+#
+# training_data = CustomDataset(
+#     csv_dir="C:\\Users\\dengchun\\Desktop\\temp.csv",
+#     transform=create_dataset()[0].reshape(-1, 1, 2),
+#     target_transform= create_dataset()[1].reshape(-1, 1, 1)
+# )
+#
+# # Download test data from open datasets.
+# test_data = CustomDataset(
+#     csv_dir="C:\\Users\\dengchun\\Desktop\\temp.csv",
+#     transform=create_dataset()[0].reshape(-1, 1, 2),
+#     target_transform=create_dataset()[1].reshape(-1, 1, 1)
+# )
+#
+# batch_size = 1
+#
+# train_dataloader = DataLoader(training_data, batch_size=batch_size)
+# test_dataloader = DataLoader(test_data, batch_size=batch_size)
+#
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# print(f"Using {device} device")
+#
+#
+# class lstm_reg(nn.Module):
+#     def __init__(self, input_size, hidden_size, output_size=1, num_layers=2):
+#         super(lstm_reg, self).__init__()
+#
+#         self.rnn = nn.LSTM(input_size, hidden_size, num_layers)  # rnn
+#         self.reg = nn.Linear(hidden_size, output_size)  # 回归
+#
+#     def forward(self, x):
+#         x, _ = self.rnn(x)  # (seq, batch, hidden)
+#         s, b, h = x.shape
+#         x = x.view(s * b, h)  # 转换成线性层的输入格式
+#         x = self.reg(x)
+#         x = x.view(s, b, -1)
+#         return x
+#
+# net = lstm_reg(2, 4).to(device)
+# criterion = nn.MSELoss()
+# optimizer = torch.optim.Adam(net.parameters(), lr=1e-2)
+# def train(dataloader, model, loss_fn, optimizer):
+#     size = len(dataloader.dataset)
+#     model.train()
+#     for batch, (X, y) in enumerate(dataloader):
+#         X, y = X.to(device), y.to(device)
+#
+#         # Compute prediction error
+#         pred = model(X)
+#         loss = loss_fn(pred, y)
+#
+#         # Backpropagation
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+#
+#         if batch % 100 == 0:
+#             loss, current = loss.item(), batch * len(X)
+#             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+#
+# def test(dataloader, model, loss_fn):
+#     size = len(dataloader.dataset)
+#     num_batches = len(dataloader)
+#     model.eval()
+#     test_loss, correct = 0, 0
+#     with torch.no_grad():
+#         for X, y in dataloader:
+#             X, y = X.to(device), y.to(device)
+#             pred = model(X)
+#             test_loss += loss_fn(pred, y).item()
+#             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+#     test_loss /= num_batches
+#     correct /= size
+#     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+#
+# epochs = 5
+# for t in range(epochs):
+#     print(f"Epoch {t+1}\n-------------------------------")
+#     train(train_dataloader, net, criterion, optimizer)
+#     test(test_dataloader, net, criterion)
+# print("Done!")
+data_csv=pd.read_csv("industry_ret_data_1500.csv")
+dataset = data_csv.values
+dataset=np.delete(dataset,0,axis=1)
+dataset = dataset.astype('float32')
+max_value = np.max(dataset,axis=0)
+min_value = np.min(dataset,axis=0)
+scalar = max_value - min_value
+dataset = list((map(lambda x: x / scalar, dataset)))
+
+
+def create_dataset(dataset, look_back=4):
+    dataX, dataY = [], []
+    for i in range(len(dataset) - look_back):
+        a = dataset[i:(i + look_back)]
+        dataX.append(a)
+        dataY.append(dataset[i + look_back])
+    return np.array(dataX), np.array(dataY)
+
+data_X, data_Y = create_dataset(dataset)
+
+train_X = data_X[:1000]
+train_Y = data_Y[:1000]
+val_X=data_X[1000:1200]
+val_Y=data_Y[1000:1200]
+test_X = data_X[1200:]
+test_Y = data_Y[1200:]
+
+train_X = train_X.reshape(-1, 433, 4)
+train_Y = train_Y.reshape(-1, 433, 1)
+test_X = test_X.reshape(-1, 433, 4)
+test_Y=test_Y.reshape(-1,433,1)
+val_X = val_X.reshape(-1, 433, 4)
+val_Y = val_Y.reshape(-1, 433, 1)
+
+train_x = torch.from_numpy(train_X)
+train_y = torch.from_numpy(train_Y)
+test_x = torch.from_numpy(test_X)
+test_y=torch.from_numpy(test_Y)
+val_x=torch.from_numpy(val_X)
+val_y = torch.from_numpy(val_Y)
+print(train_x.shape,train_y.shape)
+
+class lstm_reg(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size=1, num_layers=2):
+        super(lstm_reg, self).__init__()
+
+        self.rnn = nn.LSTM(input_size, hidden_size, num_layers)  # rnn
+        self.reg = nn.Linear(hidden_size, output_size)  # 回归
+
+    def forward(self, x):
+        x, _ = self.rnn(x)  # (seq, batch, hidden)
+        s, b, h = x.shape
+        x = x.view(s * b, h)  # 转换成线性层的输入格式
+        x = self.reg(x)
+        x = x.view(s, b, -1)
+        return x
+
+net = lstm_reg(4,4)
+
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=1e-2)
+
+# 开始训练
+tr_loss=[]
+val_loss=[]
+for e in range(100):
+    var_x = Variable(train_x)
+    var_y = Variable(train_y)
+    valx=Variable(val_x)
+    valy=Variable(val_y)
+
+    # 前向传播
+    out = net(var_x)
+    val_out=net(valx)
+    loss = criterion(out, var_y)
+    v_loss=criterion(val_out, valy)
+    tr_loss.append(loss)
+    val_loss.append(v_loss)
+    # 反向传播
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    print('Epoch: {}, Loss: {:.5f},val_Loss: {:.5f}'.format(e + 1, loss.data,v_loss))
+
+net = net.eval() # 转换成测试模式
+
+test_data = Variable(test_x)
+test_tar = Variable(test_y)
+pred_test = net(test_x) # 测试集的预测结果
+test_loss=criterion(pred_test,test_tar)
+print('ave_val_Loss: {:.5f},test_loss:{:.5f}'.format(sum(val_loss)/len(val_loss),test_loss) )
+# 改变输出的格式
+pred_test = pred_test.view(-1).data.numpy()
+a=np.zeros(4)
+pre=np.r_[a,pred_test]
+
---
+++ transform.py
@@ -0,0 +1,346 @@
+"""
+Language Modeling with nn.Transformer and TorchText
+===============================================================
+This is a tutorial on training a sequence-to-sequence model that uses the
+`nn.Transformer <https://pytorch.org/docs/stable/generated/torch.nn.Transformer.html>`__ module.
+The PyTorch 1.2 release includes a standard transformer module based on the
+paper `Attention is All You Need <https://arxiv.org/pdf/1706.03762.pdf>`__.
+Compared to Recurrent Neural Networks (RNNs), the transformer model has proven
+to be superior in quality for many sequence-to-sequence tasks while being more
+parallelizable. The ``nn.Transformer`` module relies entirely on an attention
+mechanism (implemented as
+`nn.MultiheadAttention <https://pytorch.org/docs/stable/generated/torch.nn.MultiheadAttention.html>`__)
+to draw global dependencies between input and output. The ``nn.Transformer``
+module is highly modularized such that a single component (e.g.,
+`nn.TransformerEncoder <https://pytorch.org/docs/stable/generated/torch.nn.TransformerEncoder.html>`__)
+can be easily adapted/composed.
+.. image:: ../_static/img/transformer_architecture.jpg
+"""
+
+######################################################################
+# Define the model
+# ----------------
+#
+
+
+######################################################################
+# In this tutorial, we train a ``nn.TransformerEncoder`` model on a
+# language modeling task. The language modeling task is to assign a
+# probability for the likelihood of a given word (or a sequence of words)
+# to follow a sequence of words. A sequence of tokens are passed to the embedding
+# layer first, followed by a positional encoding layer to account for the order
+# of the word (see the next paragraph for more details). The
+# ``nn.TransformerEncoder`` consists of multiple layers of
+# `nn.TransformerEncoderLayer <https://pytorch.org/docs/stable/generated/torch.nn.TransformerEncoderLayer.html>`__.
+# Along with the input sequence, a square attention mask is required because the
+# self-attention layers in ``nn.TransformerEncoder`` are only allowed to attend
+# the earlier positions in the sequence. For the language modeling task, any
+# tokens on the future positions should be masked. To produce a probability
+# distribution over output words, the output of the ``nn.TransformerEncoder``
+# model is passed through a linear layer followed by a log-softmax function.
+#
+
+import math
+from typing import Tuple
+import pandas as pd
+import numpy as np
+
+import torch
+from torch import nn, Tensor
+import torch.nn.functional as F
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+from torch.utils.data import dataset
+
+class TransformerModel(nn.Module):
+
+    def __init__(self, ntoken: int, d_model: int, nhead: int, d_hid: int,
+                 nlayers: int, dropout: float = 0.5):
+        super().__init__()
+        self.model_type = 'Transformer'
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
+        encoder_layers = TransformerEncoderLayer(d_model, nhead, d_hid, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
+        self.encoder = nn.Linear(ntoken,d_model)
+        self.d_model = d_model
+        self.decoder = nn.Linear(d_model, ntoken)
+
+
+    def init_weights(self) -> None:
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, src: Tensor, src_mask: Tensor) -> Tensor:
+        """
+        Args:
+            src: Tensor, shape [seq_len, batch_size]
+            src_mask: Tensor, shape [seq_len, seq_len]
+        Returns:
+            output Tensor of shape [seq_len, batch_size, ntoken]
+        """
+        src=src.reshape(-1,433,1)
+        src = self.encoder(src.cuda()) * math.sqrt(self.d_model)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src, src_mask)
+        output = self.decoder(output)
+        return output
+
+
+def generate_square_subsequent_mask(sz: int) -> Tensor:
+    """Generates an upper-triangular matrix of -inf, with zeros on diag."""
+    return torch.triu(torch.ones(sz, sz) * float('-inf'), diagonal=1)
+
+class NeuralNet(torch.nn.Module):
+        def __init__(self, input_size, hidden_size,num_classes):
+            super(NeuralNet, self).__init__()
+            self.fc1 = nn.Linear(input_size, hidden_size)
+            self.relu = nn.ReLU()
+            self.fc2 = nn.Linear(hidden_size, num_classes)
+
+        def forward(self, x):
+            x=x.cuda()
+            out = self.fc1(x)
+            out = self.relu(out)
+            out = self.fc2(out)
+            return out
+######################################################################
+# ``PositionalEncoding`` module injects some information about the
+# relative or absolute position of the tokens in the sequence. The
+# positional encodings have the same dimension as the embeddings so that
+# the two can be summed. Here, we use ``sine`` and ``cosine`` functions of
+# different frequencies.
+#
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def batchify(data: Tensor, bsz: int) -> Tensor:
+    """Divides the data into bsz separate sequences, removing extra elements
+    that wouldn't cleanly fit.
+    Args:
+        data: Tensor, shape [N]
+        bsz: int, batch size
+    Returns:
+        Tensor of shape [N // bsz, bsz]
+    """
+    seq_len = data.size(0) // bsz
+    data = data[:seq_len * bsz]
+    data = data.view(bsz, seq_len).t().contiguous()
+    return data.to(device)
+
+#batch_size = 4
+#eval_batch_size =4
+
+data_csv=pd.read_csv("industry_ret_data_1500.csv")
+dataset = data_csv.values
+dataset=np.delete(dataset,0,axis=1)
+dataset = dataset.astype('float32')
+max_value = np.max(dataset,axis=0)
+min_value = np.min(dataset,axis=0)
+scalar = max_value - min_value
+dataset = list((map(lambda x: x / scalar, dataset)))
+vari=[]
+for i in range(433):
+    svar=np.var(np.array(dataset)[:,i])
+    print(i,svar)
+    vari.append(svar)
+print(np.mean(vari))
+train_data = torch.from_numpy(np.array(dataset[:1000]))
+val_data = torch.from_numpy(np.array(dataset[1000:1200]))
+test_data = torch.from_numpy(np.array(dataset[1200:]))
+#train_data = batchify(train_data[:,0], batch_size)  # shape [seq_len, batch_size]
+#val_data = batchify(val_data[:,0], eval_batch_size)
+#test_data = batchify(test_data[:,0], eval_batch_size)
+
+
+######################################################################
+# Functions to generate input and target sequence
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+######################################################################
+# ``get_batch()`` generates a pair of input-target sequences for
+# the transformer model. It subdivides the source data into chunks of
+# length ``bptt``. For the language modeling task, the model needs the
+# following words as ``Target``. For example, with a ``bptt`` value of 2,
+# we’d get the following two Variables for ``i`` = 0:
+#
+# .. image:: ../_static/img/transformer_input_target.png
+#
+# It should be noted that the chunks are along dimension 0, consistent
+# with the ``S`` dimension in the Transformer model. The batch dimension
+# ``N`` is along dimension 1.
+#
+
+bptt = 42
+def get_batch(source: Tensor, i: int) -> Tuple[Tensor, Tensor]:
+    """
+    Args:
+        source: Tensor, shape [full_seq_len, batch_size]
+        i: int
+    Returns:
+        tuple (data, target), where data has shape [seq_len, batch_size] and
+        target has shape [seq_len * batch_size]
+    """
+    seq_len = min(bptt, len(source) - 1 - i)
+    data = source[i:i+seq_len]
+    target = source[i+1:i+1+seq_len].reshape(-1)
+    return data, target
+
+
+######################################################################
+# Initiate an instance
+# --------------------
+#
+
+
+######################################################################
+# The model hyperparameters are defined below. The vocab size is
+# equal to the length of the vocab object.
+#
+
+
+ntokens = 1  # size of feature dimentional
+emsize = 4  # embedding dimension
+d_hid = 4  # dimension of the feedforward network model in nn.TransformerEncoder
+nlayers = 4  # number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+nhead = 4  # number of heads in nn.MultiheadAttention
+dropout = 0.2  # dropout probability
+model = TransformerModel(ntokens, emsize, nhead, d_hid, nlayers, dropout).to(device)
+
+
+######################################################################
+# Run the model
+# -------------
+#
+
+
+######################################################################
+# We use `CrossEntropyLoss <https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html>`__
+# with the `SGD <https://pytorch.org/docs/stable/generated/torch.optim.SGD.html>`__
+# (stochastic gradient descent) optimizer. The learning rate is initially set to
+# 5.0 and follows a `StepLR <https://pytorch.org/docs/stable/generated/torch.optim.lr_scheduler.StepLR.html>`__
+# schedule. During training, we use `nn.utils.clip_grad_norm\_ <https://pytorch.org/docs/stable/generated/torch.nn.utils.clip_grad_norm_.html>`__
+# to prevent gradients from exploding.
+#
+
+import copy
+import time
+
+criterion = torch.nn.MSELoss()
+lr = 5.0  # learning rate
+optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+
+def train(model: nn.Module) -> None:
+    model.train()  # turn on train mode
+    total_loss = 0.
+    log_interval = 200
+    start_time = time.time()
+    src_mask = generate_square_subsequent_mask(bptt).to(device)
+
+    num_batches = len(train_data) // bptt
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
+        data, targets = get_batch(train_data, i)
+        batch_size = data.size(0)
+        if batch_size != bptt:  # only on last batch
+            src_mask = src_mask[:batch_size, :batch_size]
+        loss_function = torch.nn.MSELoss()  # 正确
+        output = model(data, src_mask)
+        output = torch.mean(output, 2)
+        loss = loss_function(torch.reshape(output.cuda(), targets.size()), targets.cuda())
+
+        optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+        optimizer.step()
+
+        total_loss += loss.item()
+        if batch % log_interval == 0 and batch > 0:
+            lr = scheduler.get_last_lr()[0]
+            ms_per_batch = (time.time() - start_time) * 1000 / log_interval
+            cur_loss = total_loss / log_interval
+            ppl = math.exp(cur_loss)
+            print(f'| epoch {epoch:3d} | {batch:5d}/{num_batches:5d} batches | '
+                  f'lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | '
+                  f'loss {cur_loss:5.2f} | ppl {ppl:8.2f}')
+            total_loss = 0
+            start_time = time.time()
+
+def evaluate(model: nn.Module, eval_data: Tensor) -> float:
+    model.eval()  # turn on evaluation mode
+    total_loss = 0.
+    src_mask = generate_square_subsequent_mask(bptt).to(device)
+    with torch.no_grad():
+        for i in range(0, eval_data.size(0) - 1, bptt):
+            data, targets = get_batch(eval_data, i)
+            batch_size = data.size(0)
+            if batch_size != bptt:
+                src_mask = src_mask[:batch_size, :batch_size]
+            output = model(data, src_mask)
+            output_flat = output.view(-1, ntokens)
+            loss_function = torch.nn.MSELoss()  # 正确
+            output = torch.mean(output, 2)
+            total_loss += batch_size * loss_function(output_flat.cuda(), targets.cuda()).item()
+    return total_loss / (len(eval_data) - 1)
+
+######################################################################
+# Loop over epochs. Save the model if the validation loss is the best
+# we've seen so far. Adjust the learning rate after each epoch.
+
+best_val_loss = float('inf')
+epochs = 100
+best_model = None
+
+for epoch in range(1, epochs + 1):
+    epoch_start_time = time.time()
+    train(model)
+    val_loss = evaluate(model, val_data)
+    val_ppl = math.exp(val_loss)
+    elapsed = time.time() - epoch_start_time
+    print('-' * 89)
+    print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | '
+          f'valid loss {val_loss:.5f} | valid ppl {val_ppl:8.2f}')
+    print('-' * 89)
+
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        best_model = copy.deepcopy(model)
+
+    scheduler.step()
+
+
+######################################################################
+# Evaluate the best model on the test dataset
+# -------------------------------------------
+#
+
+test_loss = evaluate(best_model, test_data)
+test_ppl = math.exp(test_loss)
+print('=' * 89)
+print(f'| End of training | test loss {test_loss:.5f} | '
+      f'test ppl {test_ppl:.5f}')
+print('=' * 89)
